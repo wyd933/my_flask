f8e6346214a0aae191659a7861e8abc0416cdd9a
root <wyd933@163.com>
2018-05-20 22:13:17 +0800

    加入了bootstrap和moment

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 #flask对象初始化
+bootstrap = Bootstrap()
+moment = Moment()
+
 def create_app():
     app = Flask(__name__)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from . import main
+from datetime import datetime
 #注册主界面蓝本的功能
 @main.route('/', methods = ['GET', 'POST'])
 def index():
     return '<h1>Hello World</h1>'
 
-@main.route('/user/<name>', methods = ['GET', 'POST'])
-def user(name):
-    return render_template('hello.html', name=name)
+#如果加入变量，那么变量会把访问的资源全部占用，不推荐这么做？？？？个人想法
+# @main.route('/user/<name>', methods = ['GET', 'POST'])
+# def user(name):
+#     return render_template('hello.html', name=name)
+
+@main.route('/user/bootstrap/<name>', methods = ['GET', 'POST'])
+def bootstrap(name):
+    return render_template('user_bootstrap.html', name=name, current_time=datetime.utcnow())
