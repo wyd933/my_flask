21e960771dc9fad7f19939c8602673898c979bf4
root <wyd933@163.com>
2018-06-01 01:28:14 +0800

    用户注册和登录

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,38 @@ from . import auth
 from  flask import render_template, redirect, url_for, flash, request
 from .forms import LoginForm, RegistrationForm
 from ..models import User
-from flask_login import login_required, logout_user, login_user
+from flask_login import login_required, logout_user, login_user, current_user
 from .. import db
+from ..email import send_email
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+
+# @auth.before_app_first_request
+# def before_request():
+#     if current_user.is_authenticated \
+#         and not current_user.confirmed \
+#         and request.endpoint \
+#         and request.blueprint != 'auth' \
+#         and request.endpoint != 'static':
+#         return redirect(url_for("auth.unconfirmed"))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -14,7 +44,6 @@ def login():
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
-
     return render_template('auth/login.html', form=form)
 
 @auth.route('/logout', methods = ['GET', 'POST'])
@@ -27,11 +56,35 @@ def logout():
 @auth.route('/register', methods = ['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
+    if form.validvbate_on_submit():
         user = User(email = form.email.data, username = form.user_name.data, \
                     password = form.password.data)
         db.session.add(user)
-        #db.session.commit()
-        flash('now youcanlogin')
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm your Account', 'auth/email/confirm', user=user, token=token)
+        flash("You can confirm your email")
+        #flash('now youcanlogin')
         return redirect(url_for("auth.login"))
     return render_template('auth/register.html', form=form)
+
+@auth.route("/confirm/<token>")
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('YOu have confirmed your account')
+    else:
+        flash('The confirmation link is invalid or has expired')
+
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -1,12 +1,23 @@
+from flask import current_app
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import login_manager
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+class Permission:
+    FOLLOW               = 0x01
+    COMMENT              = 0x02
+    WRITE_ARTICLES       = 0x04
+    MODERATE_COMMENTS    = 0x08
+    ADMINSTER            = 0x80
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    permissions = db.Column(db.Integer)
+    default = db.Column(db.Boolean, default=False, index=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
@@ -31,6 +42,10 @@ class User(UserMixin,db.Model):
 
     #密码功能
     password_hash = db.Column(db.String(128))
+
+    #用户认证功能
+    confirmed = db.Column(db.Boolean, default=False)
+
     #设定密码访问属性
     @property
     def password(self):
@@ -42,8 +57,26 @@ class User(UserMixin,db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm' : self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+
+        if data.get('confirm') != self.id:
+            return False
+
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
     def __repr__(self):
-        return '<User %r>' %self.username
+        return '<User %r Confirmed %r>' %(self.username, self.confirmed)
 
 @login_manager.user_loader
 def load_user(user_id):
